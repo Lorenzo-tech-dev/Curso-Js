b3538faf164866db329bd11c7874bf9eaa3f130c
Lucas <lucasdesousa19@hotmail.com>
2020-07-17 15:12:41 -0300

    fix - resolvem problem in the don't show hours

--- praticando/script.js
+++ praticando/script.js
@@ -2,6 +2,11 @@ function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    var hora = data.getHours
-    msg.innerHTML = `Agora são ${hora} horas.`
+    var hora = data.getHours()
+    const txt = `Agara são ${hora} horas`
+    
+    // console.log(txt)
+    msg.innerHTML = txt
+
+
 }
