4e236f879e057b394ca6a70b970d7373508122fd
Lorenzo-tech-dev <68027800+Lorenzo-tech-dev@users.noreply.github.com>
2020-07-18 15:06:38 -0300

    finished the program

--- praticando/script.js
+++ praticando/script.js
@@ -3,10 +3,20 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    const txt = `Agara são ${hora} horas`
-    
-    // console.log(txt)
-    msg.innerHTML = txt
+    msg.innerHTML = `Agara são ${hora} horas`
+    if (hora >= 0 && hora < 12){
+        // BOM DIA
+        img.src = 'manha.png'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora <= 18) {
+        // BOA TARDE
+        img.src = 'tarde.png'
+        document.body.style.background = '#b9846f'
+    } else {
+        // BOA NOITE
+        img.src = 'noite.png'
+        document.body.style.background = '#515154'
+    }
 
 
 }
